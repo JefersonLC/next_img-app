cee462d609a5582a9a47a020db452a66fcc5ec60
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-18 17:35:20 -0500

    modified layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,19 @@
+import {DM_Sans} from 'next/font/google'
 import '../styles/globals.css';
 
 export const metadata = {
   title: 'Home'
 };
 
+const firacode = DM_Sans({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap'
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={firacode.className}>
       <body>
         <main className='main'>{children}</main>
         <footer className='footer'>Footer</footer>
