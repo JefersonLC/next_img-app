ff674bf1ded84e0aebc0651649148c7c253b2874
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-17 23:24:58 -0500

    services

--- src/services/services.js
+++ src/services/services.js
@@ -1,25 +1,35 @@
-export function loginUser(values) {
-  const headersList = {
-    'Content-Type': 'application/json'
-  };
+const PATH = 'http://localhost:8000/api';
 
-  const bodyContent = JSON.stringify(values);
-
-  fetch('http://localhost:8000/api/login', {
-    method: 'POST',
-    body: bodyContent,
-    headers: headersList
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error('Error al iniciar sesión');
-      }
-      res.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) => {
-      // Maneja errores de la solicitud HTTP
-      console.error(error);
-      // Actualiza el estado del componente o muestra mensajes de error en la interfaz de usuario
+export async function loginUser(values) {
+  try {
+    const headersList = {
+      'Content-Type': 'application/json'
+    };
+    const bodyContent = JSON.stringify(values);
+    const response = await fetch(PATH + '/login', {
+      method: 'POST',
+      body: bodyContent,
+      headers: headersList
     });
+    return response;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function registerUser(values) {
+  try {
+    const headersList = {
+      'Content-Type': 'application/json'
+    };
+    const bodyContent = JSON.stringify(values);
+    const response = await fetch(PATH + '/users', {
+      method: 'POST',
+      body: bodyContent,
+      headers: headersList
+    });
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
