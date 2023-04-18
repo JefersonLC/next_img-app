c610cc4f6b1b2e6ffc33af2e0b954a4be46194da
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-17 23:26:17 -0500

    Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,10 @@
 'use client';
 
 import { Field } from 'formik';
+import '../styles/Input.css';
 
 function Input({ ty, phl, nm }) {
-  return <Field className='input' name={nm} type={ty} placeholder={phl} />;
+  return <Field className='input' name={nm} type={ty} placeholder={phl} autoComplete='off' />;
 }
 
 export default Input;
