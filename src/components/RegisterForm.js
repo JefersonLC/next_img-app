21aafbd5a0f33d8b2a0d8769fa07936744de9a16
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-18 17:35:51 -0500

    modified RegisterForm component

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,25 +2,33 @@
 
 import { useState } from 'react';
 import { Formik, Form } from 'formik';
-import FormControl from './FormControl';
-import Alert from './Alert';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { forms } from '@/utils/forms';
 import { registerUser } from '@/services/services';
+import FormControl from './FormControl';
+import Alert from './Alert';
 import '../styles/LoginRegisterPages.css';
 
 const { initialValues, validation, fields, title } = forms.register;
 
 function RegisterForm() {
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const handleSubmit = async (values) => {
+    setError(null);
+    setLoading(true);
     const response = await registerUser(values);
     const data = await response.json();
+    setLoading(false);
     const message = data.message || 'User or email already exists';
     if (!response.ok) {
       setError(message);
       return;
     }
+    router.push('/login')
   };
 
   return (
@@ -31,9 +39,9 @@ function RegisterForm() {
       onSubmit={handleSubmit}
     >
       <>
-        {error && <Alert>{error}</Alert>}
+        {error && <Alert alert='alert-error'>{error}</Alert>}
         <Form className='form'>
-          <h3>{title}</h3>
+          <h3 className='form-h3'>{title}</h3>
           {fields.map((field) => (
             <FormControl
               key={field.name}
@@ -42,7 +50,12 @@ function RegisterForm() {
               name={field.name}
             />
           ))}
-          <button type='submit'>Register</button>
+          <div className='form-links'>
+            <Link href='/login'>Are you not registered?</Link>
+          </div>
+          <button className='btn btn-add' type='submit' disabled={loading}>
+            {loading ? 'Loading...' : 'Register'}
+          </button>
         </Form>
       </>
     </Formik>
