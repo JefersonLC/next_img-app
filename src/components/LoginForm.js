9a9925f4991f13c998f3a8e5f583831cf22698c1
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-18 17:35:41 -0500

    modified LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,25 +2,33 @@
 
 import { useState } from 'react';
 import { Formik, Form } from 'formik';
-import FormControl from './FormControl';
-import Alert from './Alert';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { forms } from '@/utils/forms';
 import { loginUser } from '@/services/services';
+import FormControl from './FormControl';
+import Alert from './Alert';
 import '../styles/LoginRegisterPages.css';
 
 const { initialValues, validation, fields, title } = forms.login;
 
 function LoginForm() {
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const handleSubmit = async (values) => {
+    setError(null);
+    setLoading(true);
     const response = await loginUser(values);
     const data = await response.json();
+    setLoading(false);
     if (!response.ok) {
       setError(data.message);
       return;
     }
     localStorage.setItem('authToken', JSON.stringify(data));
+    router.push('/')
   };
 
   return (
@@ -31,9 +39,9 @@ function LoginForm() {
       onSubmit={handleSubmit}
     >
       <>
-        {error && <Alert>{error}</Alert>}
+        {error && <Alert alert='alert-error'>{error}</Alert>}
         <Form className='form'>
-          <h3>{title}</h3>
+          <h3 className='form-h3'>{title}</h3>
           {fields.map((field) => (
             <FormControl
               key={field.name}
@@ -42,7 +50,12 @@ function LoginForm() {
               name={field.name}
             />
           ))}
-          <button type='submit'>Enviar</button>
+          <div className='form-links'>
+            <Link href='/register'>Do you already have an account?</Link>
+          </div>
+          <button className='btn btn-add' type='submit' disabled={loading}>
+            {loading ? 'Loading...' : 'Ok'}
+          </button>
         </Form>
       </>
     </Formik>
